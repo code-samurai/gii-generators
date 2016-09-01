4f6f1d006fbe93cc890029f4cfd342208b674a4e
Hamza Chouaibi <hamza.chouaibi@gmail.com>
2016-09-01 04:03:11 +0100

    init repo

---
+++ crud/default/views/index.php
@@ -0,0 +1,100 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+
+echo "<?php\n";
+?>
+
+use kartik\helpers\Html;
+use <?= $generator->indexWidgetType === 'grid' ? "kartik\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
+<?= $generator->enablePjax ? 'use yii\widgets\Pjax;' : '' ?>
+
+/* @var $this yii\web\View */
+<?= !empty($generator->searchModelClass) ? "/* @var \$searchModel " . ltrim($generator->searchModelClass, '\\') . " */\n" : '' ?>
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
+
+    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
+<?php if(!empty($generator->searchModelClass)): ?>
+<?= "    <?php " . ($generator->indexWidgetType === 'grid' ? "// " : "") ?>echo $this->render('_search', ['model' => $searchModel]); ?>
+<?php endif; ?>
+
+    <p>
+        <?= "<?= " ?>Html::a(<?= $generator->generateString('Create ' . Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>, ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php if ($generator->indexWidgetType === 'grid'): ?>
+    <?= "<?= " ?>GridView::widget([
+        'dataProvider' => $dataProvider,
+        <?= $generator->enablePjax ? "'pjax=>true',\n" : '' ?>
+    'toolbar' => [
+        [
+            'content' =>
+                Html::a('<i class="fa fa-plus"></i>', ['create'], [
+                    'title' => Yii::t('kvgrid', 'Add '<?= $generator->generateString(Inflector::singularize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>'),
+                    'class' => 'btn btn-success'
+                ]) . ' ' .
+                Html::a('<i class="fa fa-repeat"></i>', ['index'], [
+                    'class' => 'btn btn-default',
+                    'title' => Yii::t('kvgrid', 'Reset Grid')
+                ]),
+        ],
+        '{export}',
+        '{toggleData}'
+    ],
+        'panel' => [
+        'heading' => false,
+        'type' => 'default',
+        'before' => Html::a('<i class="fa fa-plus"></i> Create Address', ['create'],
+            ['class' => 'btn btn-success']),
+        'after' => Html::a('<i class="fa fa-repeat"></i> Reset Grid', ['index'], ['class' => 'btn btn-info']),
+//            'footer' => false
+    ],
+    <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n        'columns' => [\n" : "'columns' => [\n"; ?>
+            ['class' => 'kartik\grid\SerialColumn'],
+
+<?php
+$count = 0;
+if (($tableSchema = $generator->getTableSchema()) === false) {
+    foreach ($generator->getColumnNames() as $name) {
+        if (++$count < 6) {
+            echo "            '" . $name . "',\n";
+        } else {
+            echo "            // '" . $name . "',\n";
+        }
+    }
+} else {
+    foreach ($tableSchema->columns as $column) {
+        $format = $generator->generateColumnFormat($column);
+        if (++$count < 6) {
+            echo "            '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        } else {
+            echo "            // '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+        }
+    }
+}
+?>
+
+            ['class' => 'kartik\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php else: ?>
+    <?= "<?= " ?>ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model-><?= $nameAttribute ?>), ['view', <?= $urlParams ?>]);
+        },
+    ]) ?>
+<?php endif; ?>
+</div>
